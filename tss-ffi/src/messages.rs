ad3529751d3601200b83954a12c12bcad61a4efe
Aler Denisov <aler.zampillo@gmail.com>
2022-07-25 22:14:55 +0300

    feat: binaries

---
+++ tss-ffi/src/messages.rs
@@ -0,0 +1,36 @@
+
+use serde::{Deserialize, Serialize};
+
+pub type Key = String;
+
+#[allow(dead_code)]
+pub const AES_KEY_BYTES_LEN: usize = 32;
+
+#[derive(Clone, PartialEq, Debug, Serialize, Deserialize)]
+pub struct AEAD {
+    pub ciphertext: Vec<u8>,
+    pub tag: Vec<u8>,
+}
+
+#[derive(Clone, PartialEq, Debug, Serialize, Deserialize)]
+pub struct PartySignup {
+    pub number: u16,
+    pub uuid: String,
+}
+
+#[derive(Clone, PartialEq, Debug, Serialize, Deserialize)]
+pub struct Index {
+    pub key: Key,
+}
+
+#[derive(Clone, PartialEq, Debug, Serialize, Deserialize)]
+pub struct Entry {
+    pub key: Key,
+    pub value: String,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct Params {
+    pub parties: String,
+    pub threshold: String,
+}
